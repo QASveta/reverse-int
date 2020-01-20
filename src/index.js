d47fb57a45cb0f3f6d22284184b505f534a08c89
svetlana pazyko <svetlanapazyko@MacBook-Pro-svetlana.local>
2020-01-20 16:14:59 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+   if (n>0) { 
+    const intRev = n.toString().split('').reverse().join('');
+    return parseInt(intRev) * Math.sign(n);
+} 
+const intRev = n.toString().split('').reverse().join('');
+    return parseInt(intRev) * -Math.sign(n);
 }
